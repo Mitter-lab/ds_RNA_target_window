6263a1cc5cc0c743dcc30d72b55508c8bf96fdaf
Stephen Fletcher <s.fletcher@uq.edu.au>
2016-03-04 09:25:36 +1000

    Fixed plotting - using X11

--- modules/heatmap.R
+++ modules/heatmap.R
@@ -1,8 +1,11 @@
 #!/usr/bin/Rscript
 library(graphics)
+x11()
 file_name = (commandArgs(TRUE)[1])
 out_file = (commandArgs(TRUE)[2])
-dsRNA_targ = read.csv(file_name, header = FALSE, row.names=1, colClasses=c("numeric","numeric"))
+dsRNA_targ = read.csv(file_name, 
+                        header = FALSE, 
+                        row.names=1, colClasses=c("numeric","numeric"))
 z=c(dsRNA_targ[,1])
 z=matrix(z)
 my_palette <- colorRampPalette(c("red","black","purple","green"))(n = 1000)
@@ -10,5 +13,5 @@ image(z,col = my_palette, axes = FALSE)
 xspace=seq(0,1,(250/length(z)))
 xlabels = seq(0,length(z),250)
 axis(1, at= xspace, labels=xlabels,las=2)
-dev.copy(pdf,out_file,11,8)
+dev.copy(jpeg, filename="plot.jpg")
 dev.off()
